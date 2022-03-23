745ef073d41e55334ba20f5e58fcef014e0fd09c
dewey preast <webwalkin@me.com>
2022-03-22 22:19:59 -0400

    added some global variables got the window.prompt working

--- script.js
+++ script.js
@@ -1,14 +1,83 @@
+var upperCase = ["A",
+  "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var lowerCase = ["a",
+  "b","c", "d", "e","f","g",
+"h", "i", "j", "k",
+"l",
+"m",
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+var symbols =["!","@","#","$","%","^","&","*","_","-","+","="]
+var yes = true
+var no = false
+
+var passwordArray =("")
+var passwordLength =("")
+var upperCase =window.prompt("would you like use upper case letters yes or no:");
+alert ("" + upperCase)
+var lowerCase =window.prompt("would you like to use lower case letters yes or no:");
+alert("" + lowerCase)
+var numbers =window.prompt("would you like to use numbers yes or no:");
+alert("" + lowerCase)
+var symbols =window.prompt("would you like to use symbols yes or no:");
+var passwordLength =window.prompt("how long would you like your password please choose between 8 and 128 characters")
+alert("" + passwordLength)
+// function if(document.getElementById(upperCase == true){
+function passwordLength(getElementByIdpasswordLength, )
+// }
+console.log ()
+
+
+
+//<input type="checkbox" id="upperCase" name="accept" value="yes">  
+//<label for="accept"> Accept </label>
+//
+
+
+//function generateString(PasswordArray) {
+ // alert("Inside");
+
+  //let result = " ";
+  //const passwordArray = passwordArray.length;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
