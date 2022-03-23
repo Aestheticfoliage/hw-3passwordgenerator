7d5677003be481d528af105fa77c2a84d54db0b2
dewey preast <webwalkin@me.com>
2022-03-23 12:40:00 -0400

    added variables and windows alerts

--- script.js
+++ script.js
@@ -1,81 +1,156 @@
-var upperCase = ["A",
-  "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-var lowerCase = ["a",
-  "b","c", "d", "e","f","g",
-"h", "i", "j", "k",
-"l",
-"m",
-"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var symbols =["!","@","#","$","%","^","&","*","_","-","+","="]
-var yes = true
-var no = false
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
+var yes = true;
+var no = false;
 
-var passwordArray =("")
-var passwordLength =("")
-var upperCase =window.prompt("would you like use upper case letters yes or no:");
-alert ("" + upperCase)
-var lowerCase =window.prompt("would you like to use lower case letters yes or no:");
-alert("" + lowerCase)
-var numbers =window.prompt("would you like to use numbers yes or no:");
-alert("" + lowerCase)
-var symbols =window.prompt("would you like to use symbols yes or no:");
-var passwordLength =window.prompt("how long would you like your password please choose between 8 and 128 characters")
-alert("" + passwordLength)
-// function if(document.getElementById(upperCase == true){
-function passwordLength(getElementByIdpasswordLength, )
+var passwordArray = [upperCase,lowerCase,symbols,numbers];
+var passwordLength = "";
+var upperCase = window.prompt(
+  "would you like use upper case letters yes or no:"
+);
+alert("" + upperCase);
+// {
+//   if (upperCase === true);
+//   else (confirm.yes);
+//   if (yes)concat(passwordArray[upperCase]);
+//  // if (true) [passwordArray.concat + upperCase];
 // }
-console.log ()
+//let result = window.confirm();
+var lowerCase = window.prompt(
+  "would you like to use lower case letters yes or no:"
+);
+alert("" + lowerCase);
+// {
+//   if (lowerCase === true);
+//   else(confirm.yes);
+//  // if (true) [passwordArray.concat + lowerCase];
+//  if (yes)concat(passwordArray[lowerCase]);
 
+//   }
 
+//let result = window.confirm()
+var numbers = window.prompt("would you like to use numbers yes or no:");
+alert("" + numbers);
+// {
+// if (numbers === true );
+// else (confirm.yes);
+// if (yes)concat(passwordArray[numbers]);
+// //if (true) [passwordArray.concat + numbers];
+// }
+//let result = window.confirm();
+var symbols = window.prompt("would you like to use symbols yes or no:");
+alert("" + symbols);
 
-//<input type="checkbox" id="upperCase" name="accept" value="yes">  
+{
+  if (upperCase);
+  else console.log;
+  if (yes) concat(passwordArray[upperCase]);
+  if (lowerCase === true);
+  else confirm.yes;
+  // if (true) [passwordArray.concat + lowerCase];
+  if (yes) concat(passwordArray[lowerCase]);
+  if (numbers === true);
+  else confirm.yes;
+  if (yes) concat(passwordArray[numbers]);
+  //if (true) [passwordArray.concat + numbers];
+  if (symbols === true);
+  else confirm.yes;
+  //if (true) [passwordArray.concat + symbols];
+  if (yes) concat(passwordArray[symbols]);
+}
+
+// let result = window.confirm();
+var passwordLength = window.prompt(
+  "how long would you like your password please choose between 8 and 128 characters"
+);
+alert("" + passwordLength);
+//let result = window.confirm(querySelector.passwordLength)
+// function if(document.getElementById(upperCase == true){
+//function passwordLength(querySelector.passwordLength ){
+
+//}
+// }
+//function querySelector(generateString)
+//if console.log(true)
+
+//<input type="checkbox" id="upperCase" name="accept" value="yes">
 //<label for="accept"> Accept </label>
 //
 
-
 //function generateString(PasswordArray) {
- // alert("Inside");
+// alert("Inside");
 
-  //let result = " ";
-  //const passwordArray = passwordArray.length;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//let result = " ";
+//const passwordArray = passwordArray.length;
 
 // Assignment Code
 //var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 //function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+//var password = generatePassword();
+//var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+//passwordText.value = password;
 
 //}
 
