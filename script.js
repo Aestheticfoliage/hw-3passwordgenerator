7498e97aae3dad5869780edb6e7790d6fca5e961
dewey preast <webwalkin@me.com>
2022-03-28 22:25:08 -0400

    fixed and working password generator, need to add all comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var upperCase =
-  ("A",
+//GLOBAL VARIABLES
+var upperCaseList = [
+  "A",
   "B",
   "C",
   "D",
@@ -24,10 +25,11 @@ var upperCase =
   "W",
   "X",
   "Y",
-  "Z");
+  "Z",
+];
 
-var lowerCase =
-  ("a",
+var lowerCaseList = [
+  "a",
   "b",
   "c",
   "d",
@@ -52,66 +54,86 @@ var lowerCase =
   "w",
   "x",
   "y",
-  "z");
+  "z",
+];
 
-var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//console.log("number", numberList);
+var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
 
-var symbols = ("!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=");
-var yes = "true";
-var no = "false";
+function generatePassword() {
+  //Varaiables
 
-var passwordArray = [""];
+  var addCharacters = "";
+  var passwordLength = window.prompt(
+    "how long would you like your password please choose between 8 and 128 characters"
+  );
+  console.log("" + passwordLength);
 
-var passwordLength = "";
-// expression one adds selected value to password array if true
-//var expression1 = console.log(yes == this)
-var upperCase = window.prompt(
-  "would you like use upper case letters yes or no:"
-);
-console.log("" + upperCase);
-// var passwordArray = function isequal(upperCase,yes){
-//   if  (upperCase === yes){
-//     console.log(passwordArray['']);
-//     else (upperCase == yes);{console.log ('')}
-//     else (upperCase === no);{
-//       console.log('');
-      
-//     }
-//   }
+  var upperCase = window.confirm(
+    "would you like use upper case letters yes or no:"
+  );
+  console.log("" + upperCase);
 
+  //Ask to incorrect
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Enter valid number of characters");
+    //Ask the user again
+    passwordLength = window.prompt(
+      "how long would you like your password please choose between 8 and 128 characters"
+    );
+  } else {
+    if (upperCase === true) {
+      addCharacters = addCharacters + upperCaseList.join("");
+    }
+    console.log("First Time", addCharacters);
 
-var lowerCase = window.prompt(
-  "would you like to use lower case letters yes or no:"
-);
-console.log("" + lowerCase);
-var numbers = window.prompt("would you like to use numbers yes or no:");
-console.log("" + numbers);
-if (lowerCase == yes); 
+    var lowerCase = window.confirm(
+      "would you like to use lower case letters yes or no:"
+    );
+    console.log("" + lowerCase);
+    if (lowerCase === true) {
+      addCharacters = addCharacters + lowerCaseList.join("");
+    }
+    var numbers = window.confirm("would you like to use numbers yes or no:");
+    console.log("" + numbers);
+    if (numbers === true) {
+      addCharacters = addCharacters + numberList.join("");
+    }
 
-console.log([passwordArray]);
+    var symbols = window.confirm("would you like to use symbols yes or no:");
+    console.log("" + symbols);
+    if (symbols == true) {
+      addCharacters = addCharacters + symbolList.join("");
+    }
+    console.log("Final List of string ", addCharacters);
 
-var symbols = window.prompt("would you like to use symbols yes or no:");
-console.log("" + symbols);
-if (symbols == true) {
-  symbols + [passwordArray];
+    var result = "";
+    //FOr loop
+    for (var i = 0; i < passwordLength; i++) {
+      //Random char based on the addcharacter list
+      var randomNumber = Math.floor(Math.random() * addCharacters.length);
+      var randomLetter = addCharacters.charAt(randomNumber);
+      console.log(randomLetter);
+      //add all the letters
+      result = result + randomLetter;
+    }
+  }
+
+  //return the final combination of password
+  return result;
 }
-console.log([passwordArray]);
-var passwordLength = window.prompt(
-  "how long would you like your password please choose between 8 and 128 characters"
-);
-console.log("" + passwordLength);
 
 // Assignment Code
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-//function writePassword() {
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//passwordText.value = password;
-
-//}
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
